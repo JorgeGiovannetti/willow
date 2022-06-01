b2c94b384a31f1903d0e96d014802dc47b6b6949
JorgeGiovannetti <giovannetti2000@gmail.com>
2022-06-01 16:03:07 -0500

    Quadruple works as struct

--- include/willow/codegen/quadruple.hpp
+++ include/willow/codegen/quadruple.hpp
@@ -5,16 +5,12 @@
 
 namespace willow::codegen
 {
-    class Quadruple
+    struct Quadruple
     {
-    public:
-        Quadruple(std::string, std::string, std::string, std::string);
-        std::string to_string();
-
-    private:
         std::string op;
         std::string address1;
         std::string address2;
         std::string targetAddress;
+        std::string to_string();
     };
 }
